498f63279f67b8353de42e6bc84ee85cd63005ff
gabe j <jacobseng2003@gmail.com>
2021-09-14 10:57:33 -0400

    Added program

---
+++ Primes/Primes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Primes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeCount = 0;
+            for (int i = 1;  primeCount <= 1000; i++)
+            {
+                int divCount = 0;
+                for (int div = i - 1; div >= 2; div--)
+                {
+                    if (i % div == 0)
+                    {
+                        divCount++;
+                    }
+                }
+
+                if (divCount == 0)
+                {
+                    primeCount++;
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
